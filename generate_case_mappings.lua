feccbb08f7e87d77ec09d81fcb5fd8685471b6d3
Marcel Fabian Krüger <tex@2krueger.de>
2022-05-01 10:05:53 +0200

    Add mappings which include tex-text ligatures

--- generate_case_mappings.lua
+++ generate_case_mappings.lua
@@ -50,11 +50,35 @@ function gen_charclass(name, class)
   return line .. ' )'
 end
 
-function write_header(file, name)
-  file:write(string.format('LHSName "UNICODE"\n\z
-  RHSName "%s"\n\n', name))
+function write_header(file, name, left_name)
+  file:write(string.format('LHSName "%s"\n\z
+  RHSName "%s"\n\n', left_name or 'UNICODE', 'anon' or name))
 end
 
+function write_pass_tex_text(file)
+  file:write'\z
+    pass(Unicode)\n\z
+    \n\z
+    ; ligatures from Knuth\'s original CMR fonts\n\z
+    U+002D U+002D			<>	U+2013	; -- -> en dash\n\z
+    U+002D U+002D U+002D	<>	U+2014	; --- -> em dash\n\z
+    \n\z
+    U+0027			<>	U+2019	; \' -> right single quote\n\z
+    U+0027 U+0027	<>	U+201D	; \'\' -> right double quote\n\z
+    U+0022			 >	U+201D	; " -> right double quote\n\z
+    \n\z
+    U+0060			<>	U+2018	; ` -> left single quote\n\z
+    U+0060 U+0060	<>	U+201C	; `` -> left double quote\n\z
+    \n\z
+    U+0021 U+0060	<>	U+00A1	; !` -> inverted exclam\n\z
+    U+003F U+0060	<>	U+00BF	; ?` -> inverted question\n\z
+    \n\z
+    ; additions supported in T1 encoding\n\z
+    U+002C U+002C	<>	U+201E	; ,, -> DOUBLE LOW-9 QUOTATION MARK\n\z
+    U+003C U+003C	<>	U+00AB	; << -> LEFT POINTING GUILLEMET\n\z
+    U+003E U+003E	<>	U+00BB	; >> -> RIGHT POINTING GUILLEMET\n\z
+  \n'
+end
 function write_pass(file, mapping, lang)
   file:write('pass(Unicode)\n\n')
   file:write(gen_charclass('cased', cased), '\n')
@@ -127,25 +151,7 @@ function write_pass(file, mapping, lang)
     end
   end
 end
-function write_teckit(name, mapping, lang)
-  local file = assert(io.open(name .. '.map', 'w'))
-  write_header(file, name)
-  write_pass(file, mapping, lang)
-  file:close()
-  assert(0 == os.spawn{'teckit_compile', name .. '.map'})
-end
-function write_greek_upper(name, mapping, alt_iota)
-  local new_mapping = {}
-  for k,v in next, mapping do new_mapping[k] = v end
-  new_mapping[0x0345] = nil
-
-  local file = assert(io.open(name .. '.map', 'w'))
-  write_header(file, name)
-  file:write('\z
-    LHSFlags(ExpectsNFD)\n\z
-    RHSFlags(GeneratesNFD)\n\z
-  \n')
-  write_pass(file, new_mapping, lang)
+function write_pass_remove_greek_diacritics(file, alt_iota)
   file:write('\n\z
     pass(Unicode)\n\z
     Class [greek] = ( U+0370..U+03FF U+1F00..U+1FFF U+2126 )\n\z
@@ -164,19 +170,58 @@ function write_greek_upper(name, mapping, alt_iota)
   if not alt_iota then
     file:write'ypogegrammeni > U+0399\n'
   end
+end
+
+function write_teckit(name, mapping, lang, tex_text)
+  local file = assert(io.open(name .. '.map', 'w'))
+  write_header(file, name, tex_text and 'TeX-text' or 'UNICODE')
+  if tex_text then write_pass_tex_text(file) end
+  write_pass(file, mapping, lang)
   file:close()
   assert(0 == os.spawn{'teckit_compile', name .. '.map'})
 end
-write_teckit('upper', uppercase)
-write_teckit('lower', lowercase)
+function write_greek_upper(name, mapping, alt_iota, tex_text)
+  local new_mapping = {}
+  for k,v in next, mapping do new_mapping[k] = v end
+  new_mapping[0x0345] = nil
+
+  local file = assert(io.open(name .. '.map', 'w'))
+  write_header(file, name, tex_text and 'TeX-text' or 'UNICODE')
+  file:write('\z
+    LHSFlags(ExpectsNFD)\n\z
+    RHSFlags(GeneratesNFD)\n\z
+  \n')
+  if tex_text then write_pass_tex_text(file) end
+  write_pass(file, new_mapping, lang)
+  write_pass_remove_greek_diacritics(file, alt_iota)
+  file:close()
+  assert(0 == os.spawn{'teckit_compile', name .. '.map'})
+end
+write_teckit('upper', uppercase, nil)
+write_teckit('upper_tex-text', uppercase, nil, true)
+write_teckit('lower', lowercase, nil)
+write_teckit('lower_tex-text', lowercase, nil, true)
 write_teckit('upper_lt', uppercase, 'lt')
+write_teckit('upper_lt_tex-text', uppercase, 'lt', true)
 write_teckit('lower_lt', lowercase, 'lt')
+write_teckit('lower_lt_tex-text', lowercase, 'lt', true)
 -- az and tr use the same tailoring
 write_teckit('upper_aztr', uppercase, 'tr')
+write_teckit('upper_aztr_tex-text', uppercase, 'tr', true)
 write_teckit('lower_aztr', lowercase, 'tr')
+write_teckit('lower_aztr_tex-text', lowercase, 'tr', true)
 -- write_teckit('upper_az', uppercase, 'az')
+-- write_teckit('upper_az_tex-text', uppercase, 'az', true)
 -- write_teckit('lower_az', lowercase, 'az')
+-- write_teckit('lower_az_tex-text', lowercase, 'az', true)
 write_greek_upper('upper_el', uppercase, false)
+write_greek_upper('upper_el_tex-text', uppercase, false, true)
+-- write_greek_upper('lower_el', uppercase, false)
+-- write_greek_upper('lower_el_tex-text', uppercase, false, true)
+-- lower_el is the same as lower
 
+-- The two variants only apply to uppercase mappings
 write_greek_upper('upper_el-xiota', uppercase, true)
+write_greek_upper('upper_el-xiota_tex-text', uppercase, true, true)
 write_teckit('upper_de-xeszett', uppercase, 'de-xeszett')
+write_teckit('upper_de-xeszett_tex-text', uppercase, 'de-xeszett', true)
